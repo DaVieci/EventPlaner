de9a841dfeb6e80009f8121befdca016d3990233
Bela Föhrenbacher <bela.foehrenbacher@ibm.com>
2020-12-06 18:49:21 +0100

    first db connect

--- src/backend/database.ts
+++ src/backend/database.ts
@@ -1,10 +1,24 @@
 var MongoClient = require('mongodb').MongoClient;
 
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/eventplanner", function (err, db) {
-   
-     if(err) throw err;
+//Insert Document
+var insertDocument = function(db, callback){
+    var collection = db.collection('events');
 
-     //Write databse Insert/Update/Query code here..
-                
+    collection.insert({ title: 'Party', date: Date()});
+
+    db.collection('events').count(function (err, count) {
+        if (err) throw err;
+        
+        console.log('Total Rows: ' + count);
+    });
+}
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/eventplanner", function(err, db){
+    if (err) throw err;
+    console.log("it is working");
+    db.close();
+    //insertDocument(client.db('eventplanner'), function() {
+      //  db.close();
+    //});
 })
